a1be925374a2fdb4c9cab40971f98665b417bf67
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-02-23 12:24:26 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+    const html = document.documentElement;
+
+/*
+    if (html.classList.contains('light')) {
+        html.classList.remove('light');
+    } else {
+        html.classList.add('light');
+    }
+        */
+
+    html.classList.toggle('light');
+
+    // pegar a tag img
+    const img= document.querySelector("#profile img");
+
+    // substituir a imagem
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './devlinks_assets/avatar-light.png');
+    } else {
+        img.setAttribute('src', './devlinks_assets/avatar.png');
+    
+    }
+
+
+}
